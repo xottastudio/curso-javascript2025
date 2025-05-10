2576d8c5e14b8a6b048a92b988f27efad32fce17
xottastudio <xottastudio@hotmail.com>
2025-05-10 02:36:35 -0500

    file variables

--- Initial/00-hola-mundo.js
+++ Initial/00-hola-mundo.js
@@ -1,1 +1,10 @@
+// creamos comentario de una linea
+/* comentario 
+de 2 o mas 
+lineas*/
+
 console.log('hola, javascript')
+console.log(5)
+console.log(5+2)
+console.log(5*2)
+console.log(5-2)
---
+++ Initial/01-variables.js
@@ -0,0 +1,24 @@
+//var - primera forma para declarar variables, forma original, ahora ya NO se crea de esa manera
+var helloWorld = 'hola mundo'
+console.log(helloWorld)
+
+var helloWorld = 'hola soy yo'
+console.log(helloWorld)
+
+
+//let, no necesita valor inicial, son accesibles solo dentro de un bloque
+
+let helloWorld2 = 'hola mundo 2'
+console.log(helloWorld2)
+
+helloWorld2 = 'hola mundo 2, soy yo'
+console.log(helloWorld2)
+
+//const, se necesita un valor, es un valor constante y no se puede cambiar, no puede ser reasignado
+
+const helloWorld3 = 'hola mundo 3'
+console.log(helloWorld3)
+
+//esto es un error
+helloWorld3 = 'hola mundo 3, soy yo'
+console.log(helloWorld3)
