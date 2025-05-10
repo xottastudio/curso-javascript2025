cbf8996652283588486728fd426e5650d4069c64
xottastudio <xottastudio@hotmail.com>
2025-05-10 02:53:24 -0500

    file data-type

---
+++ Initial/02-data-type.js
@@ -0,0 +1,28 @@
+// cadena de texto (String)
+let nombre='xavier david'
+console.log(nombre)
+
+// numeros enteros, decimal (number) 64bites
+let age=35
+let altura = 1.50
+
+//booleanos (boolean)
+let isMale=true
+
+// undefined / variable sin valor
+let valor
+console.log(valor)
+
+// NUll / sin valor
+
+let sinValor=null
+console.log(sinValor)
+
+// symbol / valor unico
+
+let mySymbol = Symbol("mysymbol")
+ 
+// BigInt / gran numero utiliza toda la memoria
+
+let myBigInt = BigInt(21654832165443132134651313213213213465464456132)
+let myBigInt2 = 21654832165443132134651313213213213465464456132n
